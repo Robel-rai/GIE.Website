2a3497cd9f3192eb2279163e2a0645306758dd8d
Robel Abera <65665555+Robel-rai@users.noreply.github.com>
2024-06-30 14:46:20 +0300

    Added responsive to mobile systems.
    I have modified the website code to make it suitable to work with mobiles.

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,12 @@ document.getElementById("serv3").onclick = function () {
 document.getElementById("serv4").onclick = function () {
     location.href = "#"
 }
+
+
+
+hamburger = document.querySelector(".hamburger");
+
+hamburger.onclick = function () {
+    navBar = document.querySelector(".nav_link");
+    navBar.classList.toggle("active");
+}
